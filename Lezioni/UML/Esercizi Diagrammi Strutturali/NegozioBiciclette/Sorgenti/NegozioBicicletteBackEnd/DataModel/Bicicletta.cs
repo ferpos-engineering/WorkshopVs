55c19c89708dba3526a9307eadeadb88e8a1a5e2
Marco Ferrero Poschetto <mposchetto@csm.de>
2022-07-22 11:22:23 +0200

    Esercizi UML aggiunti.

---
+++ Lezioni/UML/Esercizi Diagrammi Strutturali/NegozioBiciclette/Sorgenti/NegozioBicicletteBackEnd/DataModel/Bicicletta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegozioBicicletteBackEnd.DataModel
+{
+    public class Bicicletta
+    {
+        protected StatoBici stato;
+    }
+}
